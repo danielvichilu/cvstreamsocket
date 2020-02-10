07bd3399974c3fbf384033d19205944964607b76
danielvichilu <52408180+danielvichilu@users.noreply.github.com>
2020-02-10 20:33:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,21 +11,9 @@ const io = require('socket.io')(server);
 app.use(express.static(__dirname + '/public'));
 io.sockets.on('error', e => console.log(e));
 io.sockets.on('connection', function (socket) {
-  socket.on('image11',(image)=>{
-     io.emit('data11', image)
+  socket.on('image11',(image)=>{ //receive video capture from second raspberry
+     io.emit('data11', image) //send captured image to browser
    
   });
-   socket.on('image12',(image)=>{
-     io.emit('data12', image)
-   
-  });
-  socket.on('image13',(image)=>{
-    io.emit('data13', image)
-  
- });
-    socket.on('image14',(image)=>{
-    io.emit('data14', image)
-  
- });
 });
 
