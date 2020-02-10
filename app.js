e39ea04dcb25dc0910513be8140eb9f64b547d77
danielvichilu <52408180+danielvichilu@users.noreply.github.com>
2020-02-10 20:38:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+let broadcaster;
 let server;
+let port;
   const http = require('http');
   server = http.createServer(app);
   port = process.env.PORT || 3000;
@@ -9,9 +11,18 @@ const io = require('socket.io')(server);
 app.use(express.static(__dirname + '/public'));
 io.sockets.on('error', e => console.log(e));
 io.sockets.on('connection', function (socket) {
-  socket.on('image11',(image)=>{ //receive video capture from second raspberry
-     io.emit('data11', image) //send captured image to browser
+  socket.on('image11',(image)=>{
+     io.emit('data11', image)
    
   });
+  socket.on('image21',(image)=>{
+    io.emit('data21', image)
+  
+ });
+    socket.on('image22',(image)=>{
+    io.emit('data22', image)
+  
+ });
 });
-
+server.listen(port, () => console.log(`http://localhost:${port}`+ '/mix.html',))
+                          console.log(`http://localhost:${port}`+ '/index.html',);
