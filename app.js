ac7736a8c168963f1b4578f4eb8efa55ab39a7ae
danielvichilu <52408180+danielvichilu@users.noreply.github.com>
2020-02-10 20:44:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,14 +13,18 @@ io.sockets.on('error', e => console.log(e));
 io.sockets.on('connection', function (socket) {
   socket.on('image11',(image)=>{
      io.emit('data11', image)
-   
+    });
+  
+     socket.on('image12',(image)=>{
+     io.emit('data12', image)
   });
-  socket.on('image21',(image)=>{
-    io.emit('data21', image)
+  
+  socket.on('image13',(image)=>{
+    io.emit('data13', image)
   
  });
-    socket.on('image22',(image)=>{
-    io.emit('data22', image)
+    socket.on('image14',(image)=>{
+    io.emit('data14', image)
   
  });
 });
