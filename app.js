baac1a2f6d2ea9edb2e092e0f758553f1fd1c0c1
danielvichilu <52408180+danielvichilu@users.noreply.github.com>
2020-04-24 14:38:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -16,23 +16,23 @@ io.sockets.on('connection', function (socket) {
     });
   
      socket.on('image12',(image)=>{
-     io.emit('data12', image)
+     io.emit('image12', image)
   });
   
   socket.on('image13',(image)=>{
-    io.emit('data13', image)
+    io.emit('image13', image)
   
  });
     socket.on('image14',(image)=>{
-    io.emit('data14', image)
+    io.emit('image14', image)
   
  });
     socket.on('image21',(image)=>{
-    io.emit('data21', image)
+    io.emit('image21', image)
   
  });
     socket.on('image22',(image)=>{
-    io.emit('data22', image)
+    io.emit('image22', image)
 });
 socket.on('error1',(data)=>{
   io.emit('errorr', data)
